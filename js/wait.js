cb9b2c6e873970653c51fe4c7d53baf2cc1991b4
divinirakiza <irakizadivin@gmail.com>
2022-02-28 09:00:13 +0200

    [feat]: add wait javascript util

--- js/wait.js
+++ js/wait.js
@@ -1,1 +1,5 @@
 const wait = (ms) => new Promise((resolve) => {setTimeout(ms, resolve)});
+
+(async function call() {
+    await wait(2000); // Wait for 2 seconds
+})
--- ts/wait.ts
+++ ts/wait.ts
@@ -5,5 +5,5 @@ const wait = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(
  * IIFE Exepression
  */
 (async function call() {
-    await wait(100);
+    await wait(2000); // Waiting for 2 seconds
 });
